9ef4df83f0595eb2136d9e656970b1e25e34c450
imykhailychenko <igor.c.m@ukr.net>
2022-01-23 20:59:35 +0000

    hw 2

---
+++ HomeWork_2/PayField.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_2
+{
+
+    public class PayField
+    {
+        private string EmptyCell = " . ";
+        private string FilledCell = " * ";
+
+        public int Cols { get; private set; }
+        public int Rows { get; private set; }
+
+        public PayField(int cols, int rows)
+        {
+            Cols = cols;
+            Rows = rows;
+        }
+
+        public void Render(Dictionary<int, int> dots)
+        {
+            Console.Clear();
+
+            List<string[]> table = GetFileldsList(dots);
+
+            foreach (string[] row in table)
+            {
+                int length = row.Length;
+                for(int i = 0; i < length; i++)
+                {
+                    if(i == length - 1)
+                    {
+                        Console.Write(i);
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+
+        private List<string[]> GetFileldsList(Dictionary<int, int> dots)
+        {
+            var value = new List<string[]>(Cols);
+            for(int i = 0; i < Rows; i++)
+            {
+                for(int j = 0; j < Cols; j++)
+                {
+                    bool hasCol = dots.TryGetValue(i, out int dotsRow);
+
+                    if (hasCol && dotsRow == j)
+                    {
+                        value[i][j] = FilledCell;
+                    }
+                    else
+                    {
+                        value[i][j] = EmptyCell;
+                    }
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = ReadInt("Enter size: ");
+            PayField field = new PayField(size, size / 2);
+
+            Dictionary<int, int> activePoints = new Dictionary<int, int>() { { 1, 1 } };
+            field.Render(activePoints);
+        }
+
+        static private int ReadInt(string dispalayText = "")
+        {
+            int value = 0;
+            bool isParsed = false;
+
+
+            while(isParsed != true)
+            {
+                if (dispalayText != "")
+                {
+                    Console.WriteLine(dispalayText);
+                }
+                isParsed = int.TryParse(Console.ReadLine(), out value);
+            }
+
+            return value;
+        }
+    }
+}
